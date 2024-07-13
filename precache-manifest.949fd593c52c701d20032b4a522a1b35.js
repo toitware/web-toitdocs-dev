63c815355fa8e6eb4d2a7c8540feb23040e19272
floitsch <floitsch@users.noreply.github.com>
2024-07-13 13:51:24 +0000

    deploy: 933ead314a326cc9e39d1d377b85c3279342e69f

---
+++ precache-manifest.949fd593c52c701d20032b4a522a1b35.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d21d181243888153c3e3c4182ff2a01",
+    "url": "./index.html"
+  },
+  {
+    "revision": "006d7b8a227c8ddd5802",
+    "url": "./static/css/2.a6f33b58.chunk.css"
+  },
+  {
+    "revision": "f6a17a843a713843267f",
+    "url": "./static/css/main.1d35b47c.chunk.css"
+  },
+  {
+    "revision": "006d7b8a227c8ddd5802",
+    "url": "./static/js/2.27e17bac.chunk.js"
+  },
+  {
+    "revision": "544fbdbc56da6cb79363129d8efde4f7",
+    "url": "./static/js/2.27e17bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a17a843a713843267f",
+    "url": "./static/js/main.67d04e48.chunk.js"
+  },
+  {
+    "revision": "41551eafdfe88fec7336",
+    "url": "./static/js/runtime-main.3ed93bac.js"
+  },
+  {
+    "revision": "8c687c3bab8fb62c742bab2c1f73df80",
+    "url": "./static/media/ClashDisplay-Medium.8c687c3b.woff"
+  },
+  {
+    "revision": "097b328c3911a6b80b4f033b41d36db0",
+    "url": "./static/media/logo.097b328c.svg"
+  }
+]);
